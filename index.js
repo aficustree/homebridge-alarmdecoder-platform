ef009d61627eb9eb606577fa1d97c433efc2b362
Shaun Baker <shaun@sjbaker.us>
2017-09-24 20:19:57 -0500

    Revert "testing the use of updatecharacteristic intead"
    This reverts commit a03c7a07e05bd2d7c7968ad154097a00575c609b.

--- index.js
+++ index.js
@@ -8,35 +8,17 @@ function alarmdecoderSensorAccessory(log, config) {
     this.log = log;
     this.name = config.name;
     this.port = config.port;
-    this.zones = config.zones;
 }
 
 alarmdecoderSensorAccessory.prototype = {
     getServices: function() {
-        this.services = [];
+        var services = [];
         var informationService = new Service.AccessoryInformation();
         informationService
             .setCharacteristic(Characteristic.Name, this.name)
             .setCharacteristic(Characteristic.Manufacturer, 'honeywell/dsc')
-            .setCharacteristic(Characteristic.Model, 'alarmdecoder-sensor plugin');
-        this.services.push(informationService);
-        for (var zone in this.zones) {
-            var type = this.zones[zone].type;
-            var serviceToAdd;
-            if(type == 'motion') {
-                serviceToAdd = new Service.MotionSensor();
-                serviceToAdd
-                    .setCharacteristic(Characteristic.Name, zone);
-                serviceToAdd.getCharacteristic(Characteristic.ContactSensorState)
-                    .on('get', this.getState.bind(this));
-            } else if (type == 'contact'){
-                serviceToAdd = new Service.ContactSensor();
-                serviceToAdd
-                    .setCharacteristic(Characteristic.Name, zone);
-                serviceToAdd.getCharacteristic(Characteristic.ContactSensorState)
-                    .on('get', this.getState.bind(this));
-            }
-        }
+            .setCharacteristic(Characteristic.Model, 'alarmdecoder');
+        services.push(informationService);
         return services;
     }
 };
