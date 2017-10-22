f575bbf3941994aefdc37c8d82bb24fc7950b31c
Shaun Baker <shaun@sjbaker.us>
2017-10-22 10:38:51 -0500

    temp

--- index.js
+++ index.js
@@ -8,17 +8,47 @@ function alarmdecoderSensorAccessory(log, config) {
     this.log = log;
     this.name = config.name;
     this.port = config.port;
+    this.zones = config.zones;
 }
 
 alarmdecoderSensorAccessory.prototype = {
+    getState: function(callback) {
+        
+    },
+
+    identify: function(callback) {
+		this.log("Identify requested!");
+		callback(); // success
+	},
+    
     getServices: function() {
-        var services = [];
+        this.services = [];
         var informationService = new Service.AccessoryInformation();
         informationService
             .setCharacteristic(Characteristic.Name, this.name)
             .setCharacteristic(Characteristic.Manufacturer, 'honeywell/dsc')
-            .setCharacteristic(Characteristic.Model, 'alarmdecoder');
-        services.push(informationService);
+            .setCharacteristic(Characteristic.Model, 'alarmdecoder-sensor plugin');
+        this.services.push(informationService);
+        for (var zone in this.zones) {
+            var type = this.zones[zone].type;
+            this.zones[zone].state = false; //init all zones as off to start
+            var serviceToAdd;
+            if(type == 'motion') {
+                serviceToAdd = new Service.MotionSensor();
+                serviceToAdd
+                    .setCharacteristic(Characteristic.Name, zone);
+                serviceToAdd.getCharacteristic(Characteristic.ContactSensorState)
+                    .on('get', this.getState.bind(this));
+            } else if (type == 'contact'){
+                serviceToAdd = new Service.ContactSensor();
+                serviceToAdd
+                    .setCharacteristic(Characteristic.Name, zone);
+                serviceToAdd.getCharacteristic(Characteristic.ContactSensorState)
+                    .on('get', this.getState.bind(this));
+            }
+            if(type == 'motion' || type == 'contact')
+                this.services.push(serviceToAdd);
+        }
         return services;
     }
 };
