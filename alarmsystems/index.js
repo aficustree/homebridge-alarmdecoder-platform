f232ea3944bb63c4a10b2801e0b90ad96123bb49
aficustree <shaun@sjbaker.us>
2018-12-23 11:54:36 -0600

    typo

--- alarmsystems/index.js
+++ alarmsystems/index.js
@@ -1,4 +1,4 @@
 module.exports.HoneywellDSC = require('./honeywell.js').HoneywellDSC;
-module.exports.Interlogix = require('./honeywell.js').Interlogix;
+module.exports.Interlogix = require('./interlogix.js').Interlogix;
 module.exports.AlarmZone = require('./base.js').AlarmZone;
 module.exports.AlarmBase = require('./base.js').AlarmBase;
