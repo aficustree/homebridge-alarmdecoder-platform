57942834b932413d326d0bf611f2b384d42fd05b
aficustree <shaun@sjbaker.us>
2020-05-10 13:35:53 -0500

    adding alarmdecoder debug code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Accessory, Service, Characteristic, UUIDGen;
-var debug = require('debug');
+var debug = require('debug')('alarmdecoder');
 var alarms = require('./alarmsystems');
 
 module.exports = function(homebridge){
@@ -56,7 +56,8 @@ class AlarmdecoderPlatform {
     // if cached, no publish, otherwise set publish to true
     addAccessory(accessory, publish) {
         this.log('adding accessory '+ accessory.displayName);
-        accessory.on('identify', (paired, callback) => {
+        // accessory.on('identify', (paired, callback) => {
+        accessory.on('identify', (callback) => { // fix #16
             this.log(accessory.displayName, 'Identify!!!');
             callback();
         });
